4398571e1155f0d4688ad53349fc5dd894f78ff4
TGr8Prtndr <154078660+TGr8Prtndr@users.noreply.github.com>
2024-05-26 13:59:30 +0200

    Update ConnectionDAO.java

--- DAO/ConnectionDAO.java
+++ DAO/ConnectionDAO.java
@@ -12,13 +12,14 @@ public class ConnectionDAO {
 	 * URL, LOGIN et PASS sont des constantes
 	 */
 	// À utiliser si vous êtes sur une machine personnelle :
-	//final static String URL   = "jdbc:oracle:thin:@oracle.esigelec.fr:1521:orcl";
+	final static String URL   = "jdbc:oracle:thin:@oracle.esigelec.fr:1521:orcl";
 	
 	// À utiliser si vous êtes sur une machine de l'école :
-	final static String URL   = "jdbc:oracle:thin:@oracle.esigelec.fr:1521:orcl";
+	// final static String URL   = "jdbc:oracle:thin:@//srvoracledb.intranet.int:1521/orcl.intranet.int";
+	
 
-	final static String LOGIN = "C##BDD3_8";   // remplacer les ********. Exemple C##BDD1_1
-	final static String PASS  = "BDD38";   // remplacer les ********. Exemple BDD11
+	final static String LOGIN = "C##BDD3_8";   
+	final static String PASS  = "BDD38";   
 	
 	/**
 	 * Constructor
